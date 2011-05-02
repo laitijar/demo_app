062f710ae29920691ee0df754efb3ad1a10bf116
Jari Laitinen <laitijar@gmail.com>
2011-05-02 11:43:25 +0300

    Eka init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'fb3f24e0282536bc34c8cfe13e51c69c30e10e20dfb053be01d87a3e64d48bd6cb75d79785c87b6a57d2b91195e83d8c1f52442efc7d1ec3b706a68bffc9f992'
